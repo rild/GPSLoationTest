de107a18fa0275a84de0decc38650c7ab241bff9
Iida Taiki <rild3178ata@gmail.com>
2016-03-30 07:24:47 +0900

    change

--- app/src/main/java/rimp/rild/com/android/gpsloationtest/MainActivity.java
+++ app/src/main/java/rimp/rild/com/android/gpsloationtest/MainActivity.java
@@ -1,69 +1,148 @@
 package rimp.rild.com.android.gpsloationtest;
 
+import android.app.Service;
 import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity implements LocationListener{
+    private static final String TAG = MainActivity.class.getSimpleName();
+    // 更新時間(目安)
+    private static final int LOCATION_UPDATE_MIN_TIME = 0;
+    // 更新距離(目安)
+    private static final int LOCATION_UPDATE_MIN_DISTANCE = 0;
+
     // GPS用
     private LocationManager mLocationManager;
-
-    Button mGpsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.e(TAG, "onCreate");
         setContentView(R.layout.activity_main);
 
-        mGpsButton = (Button) findViewById(R.id.gps_button);
-
-        mGpsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onBtnGpsClicked();
-            }
-        });
-
-        // GPS
-        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        boolean gpsFlg = mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-        Log.d("GPS Enabled", gpsFlg ? "OK" : "NG");
+        mLocationManager = (LocationManager)this.getSystemService(Service.LOCATION_SERVICE);
+        requestLocationUpdates();
     }
 
-    // GPSボタン
-    public void onBtnGpsClicked() {
-        mLocationManager.requestLocationUpdates(
-                LocationManager.GPS_PROVIDER, //LocationManager.NETWORK_PROVIDER,
-                3000, // 通知のための最小時間間隔（ミリ秒）
-                10, // 通知のための最小距離間隔（メートル）
-                new LocationListener() {
-                    @Override
-                    public void onLocationChanged(Location location) {
-                        String msg = "Lat=" + location.getLatitude()
-                                + "\nLng=" + location.getLongitude();
-                        Log.d("GPS", msg);
-                        mLocationManager.removeUpdates(this);
-                    }
+    // Called when the location has changed.
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.e(TAG, "onLocationChanged.");
+        showLocation(location);
+    }
 
-                    @Override
-                    public void onProviderDisabled(String provider) {
-                    }
-
-                    @Override
-                    public void onProviderEnabled(String provider) {
-                    }
-
-                    @Override
-                    public void onStatusChanged(String provider, int status, Bundle extras) {
-                    }
+    // Called when the provider status changed.
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        Log.e(TAG, "onStatusChanged.");
+        showProvider(provider);
+        switch (status) {
+            case LocationProvider.OUT_OF_SERVICE:
+                // if the provider is out of service, and this is not expected to change in the near future.
+                String outOfServiceMessage = provider +"が圏外になっていて取得できません。";
+                showMessage(outOfServiceMessage);
+                break;
+            case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                // if the provider is temporarily unavailable but is expected to be available shortly.
+                String temporarilyUnavailableMessage = "一時的に" + provider + "が利用できません。もしかしたらすぐに利用できるようになるかもです。";
+                showMessage(temporarilyUnavailableMessage);
+                break;
+            case LocationProvider.AVAILABLE:
+                // if the provider is currently available.
+                if (provider.equals(LocationManager.NETWORK_PROVIDER)) {
+                    String availableMessage = provider + "が利用可能になりました。";
+                    showMessage(availableMessage);
+                    requestLocationUpdates();
                 }
-
-        );
+                break;
+        }
     }
+
+    // Called when the provider is enabled by the user.
+    @Override
+    public void onProviderEnabled(String provider) {
+        Log.e(TAG, "onProviderEnabled.");
+        String message = provider + "が有効になりました。";
+        showMessage(message);
+        showProvider(provider);
+        if (provider.equals(LocationManager.NETWORK_PROVIDER)) {
+            requestLocationUpdates();
+        }
+    }
+
+    // Called when the provider is disabled by the user.
+    @Override
+    public void onProviderDisabled(String provider) {
+        Log.e(TAG, "onProviderDisabled.");
+        showProvider(provider);
+        if (provider.equals(LocationManager.NETWORK_PROVIDER)) {
+            String message = provider + "が無効になってしまいました。";
+            showMessage(message);
+        }
+    }
+
+    private void requestLocationUpdates() {
+        Log.e(TAG, "requestLocationUpdates()");
+        showProvider(LocationManager.NETWORK_PROVIDER);
+        boolean isNetworkEnabled = mLocationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        showNetworkEnabled(isNetworkEnabled);
+        if (isNetworkEnabled) {
+            mLocationManager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER,
+                    LOCATION_UPDATE_MIN_TIME,
+                    LOCATION_UPDATE_MIN_DISTANCE,
+                    this);
+            Location location = mLocationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            if (location != null) {
+                showLocation(location);
+            }
+        } else {
+            String message = "Networkが無効になっています。";
+            showMessage(message);
+        }
+    }
+
+    private void showLocation(Location location) {
+        double longitude = location.getLongitude();
+        double latitude = location.getLatitude();
+        long time = location.getTime();
+        Date date = new Date(time);
+        DateFormat formatter = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss:SSS");
+        String dateFormatted = formatter.format(date);
+        TextView longitudeTextView = (TextView)findViewById(R.id.longitude);
+        longitudeTextView.setText("Longitude : " + String.valueOf(longitude));
+        TextView latitudeTextView = (TextView)findViewById(R.id.latitude);
+        latitudeTextView.setText("Latitude : " + String.valueOf(latitude));
+        TextView geoTimeTextView = (TextView)findViewById(R.id.geo_time);
+        geoTimeTextView.setText("取得時間 : " + dateFormatted);
+    }
+
+    private void showMessage(String message) {
+        TextView textView = (TextView)findViewById(R.id.message);
+        textView.setText(message);
+    }
+
+    private void showProvider(String provider) {
+        TextView textView = (TextView)findViewById(R.id.provider);
+        textView.setText("Provider : " + provider);
+    }
+
+    private void showNetworkEnabled(boolean isNetworkEnabled) {
+        TextView textView = (TextView)findViewById(R.id.enabled);
+        textView.setText("NetworkEnabled : " + String.valueOf(isNetworkEnabled));
+    }
+
 }
